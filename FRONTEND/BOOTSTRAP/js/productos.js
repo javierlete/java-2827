5d3d8badd5292c16c5e83c61634c47526627876a
Javier Lete <javier@eskura21.com>
2025-02-12 10:34:30 +0100

    Administración con JavaScript en Bootstrap

--- FRONTEND/BOOTSTRAP/js/productos.js
+++ FRONTEND/BOOTSTRAP/js/productos.js
@@ -1,6 +1,15 @@
 'use strict';
 
 addEventListener('DOMContentLoaded', async () => {
+    fichas();
+});
+
+function ver(seccion) {
+    document.querySelectorAll('main>section').forEach(section => section.style.display = 'none');
+    document.getElementById(seccion).style.display = 'block';
+}
+
+async function fichas() {
     const respuesta = await fetch('json/productos.json');
 
     console.log(respuesta);
@@ -9,7 +18,9 @@ addEventListener('DOMContentLoaded', async () => {
 
     console.log(productos);
 
-    const fichas = document.querySelector('#fichas>div');
+    const divFichas = document.querySelector('#fichas>div');
+
+    divFichas.innerHTML = '';
 
     productos.forEach(producto => {
         console.log(producto);
@@ -32,6 +43,45 @@ addEventListener('DOMContentLoaded', async () => {
                 </div>
             `;
 
-        fichas.appendChild(col);
+        divFichas.appendChild(col);
     });
-});
+
+    ver('fichas');
+}
+
+async function admin() {
+    const respuesta = await fetch('json/productos.json');
+
+    console.log(respuesta);
+
+    const productos = await respuesta.json();
+
+    console.log(productos);
+
+    const tbody = document.querySelector('#admin tbody');
+
+    tbody.innerHTML = '';
+
+    productos.forEach(producto => {
+        console.log(producto);
+
+        const tr = document.createElement('tr');
+
+        tr.innerHTML =
+            `
+                <td>${producto.id}</td>
+                <td>${producto.nombre}</td>
+                <td>${producto.descripcion}</td>
+                <td>${producto.precio}</td>
+                <td>${producto.stock}</td>
+                <td>
+                    <button class="btn btn-primary">Editar</button>
+                    <button class="btn btn-danger">Eliminar</button>
+                </td>
+            `;
+
+        tbody.appendChild(tr);
+    });
+
+    ver('admin');
+}
