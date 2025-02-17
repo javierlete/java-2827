8a342aba8b18ef01fc986f57776d368c587579ba
Javier Lete <javier@eskura21.com>
2025-02-17 11:24:27 +0100

    Ejemplo de botón con estado

---
+++ FRONTEND/REACT/my-app/app/MyButton.js
@@ -0,0 +1,17 @@
+'use client'; // NEXTJS me lo pide
+
+import { useState } from "react";
+
+export default function MyButton() {
+    const [count, setCount] = useState(0);
+  
+    function handleClick() {
+      setCount(count + 1);
+    }
+  
+    return (
+      <button onClick={handleClick}>
+        Clicked {count} times
+      </button>
+    );
+  }
