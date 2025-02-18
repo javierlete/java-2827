98fd58f22aae1ca91d93a562b4ab3000fde59181
Javier Lete <javier@eskura21.com>
2025-02-18 09:20:56 +0100

    Maqueta inicial

--- FRONTEND/REACT/thinking-in-react/src/App.js
+++ FRONTEND/REACT/thinking-in-react/src/App.js
@@ -1,8 +1,70 @@
-
-function App() {
+function ProductCategoryRow() {
   return (
-    <h1>Prueba</h1>
+    <tr>
+      <th colSpan="2">
+        La categoría
+      </th>
+    </tr>
   );
 }
 
-export default App;
+function ProductRow() {
+  return (
+    <tr>
+      <td>
+        <span style={{ color: 'red' }}>
+          El producto
+        </span>
+      </td>
+      <td>1234,56€</td>
+    </tr>
+  );
+}
+
+function ProductTable() {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>
+        <ProductCategoryRow />
+        <ProductRow />
+        <ProductRow />
+        <ProductRow />
+        <ProductCategoryRow />
+        <ProductRow />
+        <ProductRow />
+      </tbody>
+    </table>
+  );
+}
+
+function SearchBar() {
+  return (
+    <form>
+      <input type="text" placeholder="Search..." />
+      <label>
+        <input type="checkbox" />
+        {' '}
+        Only show products in stock
+      </label>
+    </form>
+  );
+}
+
+function FilterableProductTable() {
+  return (
+    <div>
+      <SearchBar />
+      <ProductTable />
+    </div>
+  );
+}
+
+export default function App() {
+  return <FilterableProductTable />;
+}
