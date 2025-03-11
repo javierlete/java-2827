46501c19a9f2700a0ff2d3c2e5c3536c50bf8deb
Javier Lete <javier@eskura21.com>
2025-03-11 10:07:14 +0100

    Base de datos con SonarQube pasado

--- BACKEND/JDBC/src/basesdedatos/BasesDeDatos.java
+++ BACKEND/JDBC/src/basesdedatos/BasesDeDatos.java
@@ -11,89 +11,98 @@ import java.time.LocalDate;
 import java.util.Scanner;
 
 public class BasesDeDatos {
-	public static void main(String[] args) throws SQLException, ClassNotFoundException {
+	public static void main(String[] args) throws ClassNotFoundException {
 		Scanner sc = new Scanner(System.in);
-		
-		String url = "jdbc:mysql://localhost:3306/2827_tienda";
-		String user = "root";
-		String pass = "1234";
 
-		String sqlSelect = "SELECT * FROM productos";
-		String sqlSelectId = "SELECT * FROM productos WHERE id=?";
-		String sqlInsert = "INSERT INTO productos (nombre, precio, caducidad, descripcion) VALUES (?,?,?,?)";
+		String url = System.getenv("JDBC_URL");
+		String user = System.getenv("JDBC_USER");
+		String pass = System.getenv("JDBC_PASS");
+
+		System.out.println(user);
+		System.out.println(pass);
 		
+		String sqlSelect = "SELECT id, nombre, precio, caducidad, descripcion FROM productos";
+		String sqlSelectId = sqlSelect + " WHERE id=?";
+		String sqlInsert = "INSERT INTO productos (nombre, precio, caducidad, descripcion) VALUES (?,?,?,?)";
+
 		@SuppressWarnings("unused")
 		String sqlUpdate = "UPDATE productos SET nombre=?, precio=?, caducidad=?, descripcion=? WHERE id=?";
-		
+
 		@SuppressWarnings("unused")
 		String sqlDelete = "DELETE FROM productos WHERE id=?";
 
 		System.out.println("Conectando a la base de datos");
 
-		Class.forName("com.mysql.cj.jdbc.Driver");
-		
-		Connection con = DriverManager.getConnection(url, user, pass);
-		Statement st = con.createStatement();
-		ResultSet rs = st.executeQuery(sqlSelect);
+		Class.forName(System.getenv("JDBC_DRIVER"));
 
-		System.out.println("TODOS");
-		System.out.println("=====");
-		
-		while (rs.next()) {
-			System.out.println(rs.getString("id"));
-			System.out.println(rs.getString("nombre"));
-			System.out.println(rs.getString("precio"));
-			System.out.println(rs.getString("caducidad"));
-			System.out.println(rs.getString("descripcion"));
+		int numeroRegistrosModificados = 0;
+
+		try (Connection con = DriverManager.getConnection(url, user, pass)) {
+			try (Statement st = con.createStatement(); ResultSet rs = st.executeQuery(sqlSelect);) {
+
+				System.out.println("TODOS");
+				System.out.println("=====");
+
+				while (rs.next()) {
+					System.out.println(rs.getString("id"));
+					System.out.println(rs.getString("nombre"));
+					System.out.println(rs.getString("precio"));
+					System.out.println(rs.getString("caducidad"));
+					System.out.println(rs.getString("descripcion"));
+				}
+			}
+
+			System.out.print("Dime el id: ");
+
+			String id = sc.nextLine();
+
+			try (PreparedStatement pst = con.prepareStatement(sqlSelectId)) {
+				pst.setLong(1, Long.parseLong(id));
+
+				ResultSet rs = pst.executeQuery();
+
+				System.out.println("UNO");
+				System.out.println("===");
+
+				while (rs.next()) {
+					System.out.println(rs.getString("id"));
+					System.out.println(rs.getString("nombre"));
+					System.out.println(rs.getString("precio"));
+					System.out.println(rs.getString("caducidad"));
+					System.out.println(rs.getString("descripcion"));
+				}
+
+			}
+
+			System.out.println("INSERTAR");
+			System.out.println("========");
+
+			System.out.print("Nombre: ");
+			String nombre = sc.nextLine();
+
+			System.out.print("Precio: ");
+			BigDecimal precio = new BigDecimal(sc.nextLine());
+
+			System.out.print("Caducidad: ");
+			LocalDate caducidad = LocalDate.parse(sc.nextLine());
+
+			System.out.print("Descripción: ");
+			String descripcion = sc.nextLine();
+
+			try (PreparedStatement pst = con.prepareStatement(sqlInsert)) {
+				pst.setString(1, nombre);
+				pst.setBigDecimal(2, precio);
+				pst.setDate(3, java.sql.Date.valueOf(caducidad));
+				pst.setString(4, descripcion);
+
+				numeroRegistrosModificados = pst.executeUpdate();
+			}
+		} catch (NumberFormatException | SQLException e) {
+			System.out.println("Ha habido un error en la conexión a la base de datos");
 		}
-		
-		System.out.print("Dime el id: ");
-		
-		String id = sc.nextLine();
-		
-		PreparedStatement pst = con.prepareStatement(sqlSelectId);
-		
-		pst.setLong(1, Long.parseLong(id));
-		
-		rs = pst.executeQuery();
-		
-		System.out.println("UNO");
-		System.out.println("===");
-		
-		while (rs.next()) {
-			System.out.println(rs.getString("id"));
-			System.out.println(rs.getString("nombre"));
-			System.out.println(rs.getString("precio"));
-			System.out.println(rs.getString("caducidad"));
-			System.out.println(rs.getString("descripcion"));
-		}
-		
-		System.out.println("INSERTAR");
-		System.out.println("========");
-		
-		System.out.print("Nombre: ");
-		String nombre = sc.nextLine();
-		
-		System.out.print("Precio: ");
-		BigDecimal precio = new BigDecimal(sc.nextLine());
-		
-		System.out.print("Caducidad: ");
-		LocalDate caducidad = LocalDate.parse(sc.nextLine());
-		
-		System.out.print("Descripción: ");
-		String descripcion = sc.nextLine();
-		
-		pst = con.prepareStatement(sqlInsert);
-		
-		pst.setString(1, nombre);
-		pst.setBigDecimal(2, precio);
-		pst.setDate(3, java.sql.Date.valueOf(caducidad));
-		pst.setString(4, descripcion);
-		
-		int numeroRegistrosModificados = pst.executeUpdate();
-		
+
 		System.out.println(numeroRegistrosModificados);
-		
+
 		sc.close();
 	}
 }
