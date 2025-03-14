de9a479052f3fc1c6ec10a5bcf309a6a0aa687ad
Javier Lete <javier@eskura21.com>
2025-03-14 11:36:10 +0100

    Ejemplo de import static con clase Math

---
+++ BACKEND/JDBC/src/bases/Matematicas.java
@@ -0,0 +1,12 @@
+package bases;
+
+import static java.lang.Math.*;
+
+public class Matematicas {
+	public static void main(String[] args) {
+		System.out.println(5^2);
+		
+		System.out.println(pow(5, 2));
+		System.out.println(sqrt(25));
+	}
+}
