2670eeacefbfc8295cf1286d3bb8305946a34037
Javier Lete <javier@eskura21.com>
2025-03-24 10:57:03 +0100

    Añadimos constructor de tres parámetros para la fábrica

--- BACKEND/JDBC/src/accesodatos/ProductoDaoFake.java
+++ BACKEND/JDBC/src/accesodatos/ProductoDaoFake.java
@@ -6,45 +6,49 @@ import java.util.ArrayList;
 import entidades.Producto;
 
 class ProductoDaoFake implements ProductoDao {
+	public ProductoDaoFake(String ignorado1, String ignorado2, String ignorado3) {
+
+	}
+
 	@Override
 	public Iterable<Producto> obtenerTodos() {
 		System.out.println("OBTENER TODOS LOS PRODUCTOS");
-		
+
 		return new ArrayList<>();
 	}
 
 	@Override
 	public Producto obtenerPorId(Long id) {
 		System.out.println("OBTENER PRODUCTO ID " + id);
-		
+
 		return new Producto();
 	}
 
 	@Override
 	public Iterable<Producto> buscarPorNombre(String nombre) {
 		System.out.println("BUSCAR POR NOMBRE " + nombre);
-		
+
 		return new ArrayList<>();
 	}
 
 	@Override
 	public Iterable<Producto> buscarPorPrecio(BigDecimal minimo, BigDecimal maximo) {
-System.out.println("BUSCAR POR PRECIO ENTRE " + minimo + " Y " + maximo);
-		
+		System.out.println("BUSCAR POR PRECIO ENTRE " + minimo + " Y " + maximo);
+
 		return new ArrayList<>();
 	}
 
 	@Override
 	public Producto insertar(Producto producto) {
 		System.out.println("INSERTANDO " + producto);
-		
+
 		return producto;
 	}
 
 	@Override
 	public Producto modificar(Producto producto) {
 		System.out.println("MODIFICANDO " + producto);
-		
+
 		return producto;
 	}
 
