c1b84cec1d48f528374dff68a37586c43de08dcf
Javier Lete <javier@eskura21.com>
2025-03-26 11:50:05 +0100

    Ejemplos de lambda para los eventos de Swing

--- BACKEND/JDBC/src/bases/SwingEjemplo.java
+++ BACKEND/JDBC/src/bases/SwingEjemplo.java
@@ -2,6 +2,8 @@ package bases;
 
 import java.awt.Container;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -10,6 +12,8 @@ import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
 public class SwingEjemplo {
+	private static JLabel l;
+	
 	public static void main(String[] args) throws ClassNotFoundException, InstantiationException,
 			IllegalAccessException, UnsupportedLookAndFeelException {
 		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
@@ -20,14 +24,58 @@ public class SwingEjemplo {
 		Container contenido = ventana.getContentPane();
 		contenido.setLayout(new FlowLayout());
 		
-		JLabel l = new JLabel("Hola");
+//		Escuchador
+//		JLabel l = new JLabel("Hola"); // NOSONAR
+		
+//		Escuchador2
+		l = new JLabel("Hola");
+		
 		JButton b = new JButton("Púlsame");
 		
 		contenido.add(l);
 		contenido.add(b);
 		
-		b.addActionListener(e -> l.setText("PULSADO"));
+//		Escuchador e = new Escuchador(l); // NOSONAR
+//		b.addActionListener(e); // NOSONAR
 		
+//		Escuchador2
+//		b.addActionListener(new Escuchador2()); // NOSONAR
+		
+//		CLASE ANÓNIMA
+//		NOSONAR {
+//		b.addActionListener(new ActionListener() {
+//			@Override
+//			public void actionPerformed(ActionEvent e) {
+//				l.setText("Pulsado desde clase ANÓNIMA");
+//			}
+//		});
+//		}
+		
+		// LAMBDA
+		b.addActionListener(e -> l.setText("Pulsado desde lambda"));
+
 		ventana.setVisible(true);
 	}
+	
+	// CLASE INTERNA
+	static class Escuchador2 implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			l.setText("Pulsado");
+		}
+	}
+}
+
+class Escuchador implements ActionListener {
+	private JLabel label;
+	
+	Escuchador(JLabel label) {
+		this.label = label;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		label.setText("Pulsado");
+	}
+	
 }
