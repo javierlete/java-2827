d74b39485462bd01777a99eea4cfc28cbdd6a6b2
Javier Lete <javier@eskura21.com>
2025-03-27 09:24:45 +0100

    Correcciones de Sonar para el ejemplo de WindowBuilder

--- BACKEND/JDBC/src/bases/SwingConWindowBuilder.java
+++ BACKEND/JDBC/src/bases/SwingConWindowBuilder.java
@@ -2,33 +2,29 @@ package bases;
 
 import java.awt.EventQueue;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import javax.swing.WindowConstants;
 
 public class SwingConWindowBuilder {
 
 	private JFrame frame;
-	private JTextField tfNombre;
 	private JLabel lblResultado;
 
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					SwingConWindowBuilder window = new SwingConWindowBuilder();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
+		EventQueue.invokeLater(() -> {
+			try {
+				SwingConWindowBuilder window = new SwingConWindowBuilder();
+				window.frame.setVisible(true);
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
 		});
 	}
@@ -53,27 +49,23 @@ public class SwingConWindowBuilder {
 	private void initialize() {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 300, 102);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		
+
 		JLabel lblNewLabel = new JLabel("Nombre");
 		lblNewLabel.setBounds(10, 11, 46, 14);
 		frame.getContentPane().add(lblNewLabel);
-		
-		tfNombre = new JTextField();
+
+		JTextField tfNombre = new JTextField();
 		tfNombre.setBounds(66, 8, 86, 20);
 		frame.getContentPane().add(tfNombre);
 		tfNombre.setColumns(10);
-		
+
 		JButton btnSaludar = new JButton("Saludar");
-		btnSaludar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				lblResultado.setText("Hola " + tfNombre.getText());
-			}
-		});
+		btnSaludar.addActionListener(e -> lblResultado.setText("Hola " + tfNombre.getText()));
 		btnSaludar.setBounds(162, 7, 89, 23);
 		frame.getContentPane().add(btnSaludar);
-		
+
 		lblResultado = new JLabel("");
 		lblResultado.setBounds(10, 36, 243, 14);
 		frame.getContentPane().add(lblResultado);
