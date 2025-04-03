7da1d2d1d449918c7c210fa21c4705e209db49f1
Javier Lete <javier@eskura21.com>
2025-04-03 09:49:08 +0200

    Maven/New Maven Module Project accesodatos

---
+++ multimodulo/accesodatos/src/main/java/com/ipartek/formacion/multimodulo/accesodatos/App.java
@@ -0,0 +1,10 @@
+package com.ipartek.formacion.multimodulo.accesodatos;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
