f2eba4036709488f65fc441785d9116a12ac7688
Javier Lete <javier@eskura21.com>
2025-04-03 10:10:59 +0200

    New/Maven/Maven Project bibliotecas con copia de ficheros

---
+++ bibliotecas/src/main/java/com/ipartek/formacion/bibliotecas/Fabrica.java
@@ -0,0 +1,33 @@
+package com.ipartek.formacion.bibliotecas;
+
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Properties;
+
+public class Fabrica {
+	private Fabrica() {
+	}
+
+	private static Properties props; 
+	
+	static {
+		try {
+			props = new Properties();
+			props.load(Fabrica.class.getClassLoader().getResourceAsStream("basesdedatos.properties"));
+		} catch (IOException e) {
+			throw new AccesoDatosException("No se ha podido leer el fichero de configuración", e);
+		}
+	}
+
+	public static Object getObject(String nombre) {
+		try {
+			Class<?> clase = Class.forName(props.getProperty(nombre));
+			Constructor<?> constructor3String = clase.getConstructor(String.class, String.class, String.class);
+			return constructor3String.newInstance(System.getenv("JDBC_URL"), System.getenv("JDBC_USER"), System.getenv("JDBC_PASS"));
+		} catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException
+				| NoSuchMethodException | SecurityException | ClassNotFoundException e) {
+			throw new AccesoDatosException("No se ha podido crear el DAO " + nombre);
+		}
+	}
+}
