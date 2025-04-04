28b83f4277fed586a6ddd10fb398bf06bfcbd1eb
Javier Lete <javier@eskura21.com>
2025-04-04 08:46:13 +0200

    Aplicación de consola básica

---
+++ multimodulo/presentacion-consola/src/main/java/com/ipartek/formacion/multimodulo/presentacion/consola/App.java
@@ -0,0 +1,57 @@
+package com.ipartek.formacion.multimodulo.presentacion.consola;
+
+import java.util.Scanner;
+
+import com.ipartek.formacion.multimodulo.entidades.Producto;
+import com.ipartek.formacion.multimodulo.logicanegocio.AnonimoNegocio;
+import com.ipartek.formacion.multimodulo.logicanegocio.AnonimoNegocioImpl;
+
+public class App {
+	private static final AnonimoNegocio NEGOCIO = new AnonimoNegocioImpl();
+	
+    public static void main(String[] args) {
+    	boolean salir = false;
+		
+    	do {
+			menu();
+			int opcion = pedirOpcion();
+			procesar(opcion);
+			
+			if(opcion == 0) {
+				salir = true;
+			}
+		} while (!salir);
+    }
+
+	private static void menu() {
+		System.out.println("""
+				1. Listado
+				
+				0. Salir
+				""");
+	}
+
+	private static int pedirOpcion() {
+		return pedirEntero("Dime la opción deseada");
+	}
+
+	private static int pedirEntero(String mensaje) {
+		System.out.print(mensaje + ": ");
+		Scanner sc = new Scanner(System.in);
+		return sc.nextInt();
+	}
+
+	private static void procesar(int opcion) {
+		switch(opcion) {
+		case 1 -> listado();
+		case 0 -> System.out.println("Gracias por tu visita");
+		default -> System.out.println("No se reconoce la opción " + opcion);
+		}
+	}
+
+	private static void listado() {
+		for(Producto p: NEGOCIO.listarProductos()) {
+			System.out.println(p);
+		}
+	}
+}
