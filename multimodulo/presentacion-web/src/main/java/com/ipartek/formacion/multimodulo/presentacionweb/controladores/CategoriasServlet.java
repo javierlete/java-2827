9cb1a1428158778ed62ddf2eabf150a7ac81d1ed
Javier Lete <javier@eskura21.com>
2025-04-09 09:52:53 +0200

    Maqueta de categorías arrancada desde servlet

---
+++ multimodulo/presentacion-web/src/main/java/com/ipartek/formacion/multimodulo/presentacionweb/controladores/CategoriasServlet.java
@@ -0,0 +1,31 @@
+package com.ipartek.formacion.multimodulo.presentacionweb.controladores;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/categorias")
+public class CategoriasServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// Recoger información de la petición
+		// Convertir la información recibida
+		// Empaquetar en modelos
+		// Ejecutar lógica de negocio
+		// Preparar modelo para la siguiente vista
+		// Saltar a la siguiente vista
+		try {
+			request.getRequestDispatcher("/WEB-INF/vistas/categorias.jsp").forward(request, response);
+		} catch (ServletException | IOException e) {
+			System.err.println("Error en la petición de categorías");
+			System.err.println(e.getStackTrace());
+		}
+	}
+}
