ab4492375d20504a4f951f6ed98f850fd0e61247
Javier Lete <javier@eskura21.com>
2025-04-10 10:25:54 +0200

    Logout

---
+++ multimodulo/presentacion-web/src/main/java/com/ipartek/formacion/multimodulo/presentacionweb/controladores/LogoutServlet.java
@@ -0,0 +1,35 @@
+package com.ipartek.formacion.multimodulo.presentacionweb.controladores;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// Recoger información de la petición
+		HttpSession session = request.getSession();
+		
+		// Convertir la información recibida
+		// Empaquetar en modelos
+		// Ejecutar lógica de negocio
+		session.invalidate();
+		
+		// Preparar modelo para la siguiente vista
+		// Saltar a la siguiente vista
+		try {
+			response.sendRedirect("login");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
