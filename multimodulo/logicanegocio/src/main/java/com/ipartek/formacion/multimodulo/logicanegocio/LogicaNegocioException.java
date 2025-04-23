67b3edd2f4db861404c711570ae62901eb7b9d19
Javier Lete <javier@eskura21.com>
2025-04-23 10:13:41 +0200

    Excepciones personalizadas con lombok

--- multimodulo/logicanegocio/src/main/java/com/ipartek/formacion/multimodulo/logicanegocio/LogicaNegocioException.java
+++ multimodulo/logicanegocio/src/main/java/com/ipartek/formacion/multimodulo/logicanegocio/LogicaNegocioException.java
@@ -1,29 +1,8 @@
 package com.ipartek.formacion.multimodulo.logicanegocio;
 
+import lombok.experimental.StandardException;
+
+@StandardException
 public class LogicaNegocioException extends RuntimeException {
-
 	private static final long serialVersionUID = -7463109200298733893L;
-
-	public LogicaNegocioException() {
-		super();
-	}
-
-	public LogicaNegocioException(String message, Throwable cause, boolean enableSuppression,
-			boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
-	}
-
-	public LogicaNegocioException(String message, Throwable cause) {
-		super(message, cause);
-	}
-
-	public LogicaNegocioException(String message) {
-		super(message);
-	}
-
-	public LogicaNegocioException(Throwable cause) {
-		super(cause);
-	}
-
-	
 }
