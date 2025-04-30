4ea337c6215526746887f8a721edd36c29759608
Javier Lete <javier@eskura21.com>
2025-04-30 10:59:26 +0200

    Simplificación del acceso a JPA con biblioteca extendida

--- bibliotecas/src/main/java/com/ipartek/formacion/bibliotecas/JpaDao.java
+++ bibliotecas/src/main/java/com/ipartek/formacion/bibliotecas/JpaDao.java
@@ -7,20 +7,23 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 
-public class JpaDao {
-	public JpaDao(String unidadDePersistencia) {
+public class JpaDao<T> {
+	private Class<T> tipo;
+
+	public JpaDao(Class<T> tipo, String unidadDePersistencia) {
 		entityManagerFactory = Persistence.createEntityManagerFactory(unidadDePersistencia);
+		this.tipo = tipo;
 	}
 
 	private final EntityManagerFactory entityManagerFactory;
 
-	public <T> T enTransaccion(Function<EntityManager, T> codigo) {
+	private <G> G enTransaccion(Function<EntityManager, G> codigo) {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		EntityTransaction transaction = entityManager.getTransaction();
 
 		try {
 			transaction.begin();
-			T resultado = codigo.apply(entityManager);
+			G resultado = codigo.apply(entityManager);
 			transaction.commit();
 
 			return resultado;
@@ -33,5 +36,76 @@ public class JpaDao {
 		} finally {
 			entityManager.close();
 		}
+	}
+
+	public Iterable<T> consultaVarios(String jpql, Object... argumentos) {
+		return enTransaccion(em -> {
+			var consulta = em.createQuery(jpql, tipo);
+
+			int posicion = 1;
+
+			for (var argumento : argumentos) {
+				consulta.setParameter(posicion++, argumento);
+			}
+
+			return consulta.getResultList();
+		});
+	}
+
+	public T consultaUno(String jpql, Object... argumentos) {
+		return enTransaccion(em -> {
+			var consulta = em.createQuery(jpql, tipo);
+
+			int posicion = 1;
+
+			for (var argumento : argumentos) {
+				consulta.setParameter(posicion++, argumento);
+			}
+
+			return consulta.getSingleResultOrNull();
+		});
+	}
+
+	public int cambio(String jpql, Object... argumentos) {
+		return enTransaccion(em -> {
+			var consulta = em.createQuery(jpql, tipo);
+
+			int posicion = 1;
+
+			for (var argumento : argumentos) {
+				consulta.setParameter(posicion++, argumento);
+			}
+
+			return consulta.executeUpdate();
+		});
+	}
+
+	public Iterable<T> buscarTodos() {
+		return enTransaccion(em -> em.createQuery("from " + tipo.getSimpleName(), tipo).getResultList());
+	}
+
+	public T buscarPorId(Long id) {
+		return enTransaccion(em -> em.find(tipo, id));
+	}
+
+	public T insertar(T objeto) {
+		return enTransaccion(em -> {
+			em.persist(objeto);
+			return objeto;
+		});
+	}
+
+	public T modificar(T objeto) {
+		return enTransaccion(em -> {
+			em.merge(objeto);
+			return objeto;
+		});
+	}
+
+	public void borrar(Long id) {
+		enTransaccion(em -> {
+			em.remove(em.find(tipo, id));
+			return null;
+		});
 	}
 }
--- multimodulo/accesodatos-jpa/src/main/java/com/ipartek/formacion/multimodulo/accesodatos/jpa/ProductoDaoJpa.java
+++ multimodulo/accesodatos-jpa/src/main/java/com/ipartek/formacion/multimodulo/accesodatos/jpa/ProductoDaoJpa.java
@@ -9,43 +9,35 @@ import com.ipartek.formacion.multimodulo.entidades.Producto;
 
 public class ProductoDaoJpa implements ProductoDao {
 
-	private static final JpaDao JPA = new JpaDao("com.ipartek.formacion.multimodulo.entidades");
+	private static final JpaDao<Producto> JPA = new JpaDao<>(Producto.class,
+			"com.ipartek.formacion.multimodulo.entidades");
 
 	public ProductoDaoJpa(String noUsado1, String noUsado2, String noUsado3) {
 	}
 
 	@Override
 	public Iterable<Producto> obtenerTodos() {
-		return JPA.enTransaccion(em -> em.createQuery("from Producto", Producto.class).getResultList());
+		return JPA.buscarTodos();
 	}
 
 	@Override
 	public Producto obtenerPorId(Long id) {
-		return JPA.enTransaccion(em -> em.find(Producto.class, id));
+		return JPA.buscarPorId(id);
 	}
 
 	@Override
 	public Producto insertar(Producto producto) {
-		return JPA.enTransaccion(em -> {
-			em.persist(producto);
-			return producto;
-		});
+		return JPA.insertar(producto);
 	}
 
 	@Override
 	public Producto modificar(Producto producto) {
-		return JPA.enTransaccion(em -> {
-			em.merge(producto);
-			return producto;
-		});
+		return JPA.modificar(producto);
 	}
 
 	@Override
 	public void borrar(Long id) {
-		JPA.enTransaccion(em -> {
-			em.remove(em.find(Producto.class, id));
-			return null;
-		});
+		JPA.borrar(id);
 	}
 
 	@Override
@@ -55,15 +47,12 @@ public class ProductoDaoJpa implements ProductoDao {
 
 	@Override
 	public Iterable<Producto> buscarPorNombre(String nombre) {
-		return JPA.enTransaccion(em -> em.createQuery("from Producto p where p.nombre like :nombre", Producto.class)
-					.setParameter("nombre", "%" + nombre + "%").getResultList());
+		return JPA.consultaVarios("from Producto p where p.nombre like ?1", "%" + nombre + "%");
 	}
 
 	@Override
 	public Iterable<Producto> buscarPorPrecio(BigDecimal minimo, BigDecimal maximo) {
-		return JPA.enTransaccion(em -> em
-					.createQuery("from Producto p where p.precio between :minimo and :maximo", Producto.class)
-					.setParameter("minimo", minimo).setParameter("maximo", maximo).getResultList());
+		return JPA.consultaVarios("from Producto p where p.precio between ?1 and ?2", minimo, maximo);
 	}
 
 }
