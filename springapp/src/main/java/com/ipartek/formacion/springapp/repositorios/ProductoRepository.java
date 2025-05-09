720aba3c327715de440f33abfb650d9d720e0a76
Javier Lete <javier@eskura21.com>
2025-05-09 10:10:42 +0200

    Corrección del repositorio para findById con Categoria

--- springapp/src/main/java/com/ipartek/formacion/springapp/repositorios/ProductoRepository.java
+++ springapp/src/main/java/com/ipartek/formacion/springapp/repositorios/ProductoRepository.java
@@ -1,19 +1,26 @@
 package com.ipartek.formacion.springapp.repositorios;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.lang.NonNull;
 
 import com.ipartek.formacion.springapp.entidades.Producto;
 
 @RepositoryRestResource(collectionResourceRel = "productos", path = "productos")
 public interface ProductoRepository extends CrudRepository<Producto, Long>, PagingAndSortingRepository<Producto, Long> {
 	@EntityGraph(attributePaths = {"categoria"})
+	@NonNull
 	Iterable<Producto> findAll();
-	
+
+	@EntityGraph(attributePaths = {"categoria"})
+	@NonNull
+	Optional<Producto> findById(@NonNull Long id);
+
 	@EntityGraph(attributePaths = {"categoria"})
 	Iterable<Producto> findByNombre(String nombre);
 	
