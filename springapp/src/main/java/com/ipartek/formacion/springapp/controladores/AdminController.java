cf5d7ff8d6bca25414122726653728d66f89d822
Javier Lete <javier@eskura21.com>
2025-05-09 11:46:25 +0200

    Controladores y vistas con MVC con Thymeleaf

---
+++ springapp/src/main/java/com/ipartek/formacion/springapp/controladores/AdminController.java
@@ -0,0 +1,56 @@
+package com.ipartek.formacion.springapp.controladores;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ipartek.formacion.springapp.entidades.Producto;
+import com.ipartek.formacion.springapp.servicios.AdminService;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+	@Autowired
+	private AdminService servicio;
+
+	@GetMapping("/productos")
+	public String productos(Model modelo) {
+		modelo.addAttribute("productos", servicio.listarProductos());
+
+		return "admin/productos";
+	}
+
+	@GetMapping("/producto")
+	public String producto(Producto producto) {
+		return "admin/producto";
+	}
+
+	@GetMapping("/producto/{id}")
+	public String productoPorId(@PathVariable Long id, Model modelo) {
+		modelo.addAttribute("producto", servicio.buscarProductoPorId(id));
+
+		return "admin/producto";
+	}
+
+	@PostMapping("/producto")
+	public String guardarProducto(Producto producto) {
+		if (producto.getId() == null) {
+			servicio.anyadirProducto(producto);
+		} else {
+			servicio.modificarProducto(producto);
+		}
+
+		return "redirect:/admin/productos";
+	}
+	
+	@GetMapping("/producto/{id}/borrar")
+	public String borrarProducto(@PathVariable Long id) {
+		servicio.borrarProducto(id);
+		
+		return "redirect:/admin/productos";
+	}
+}
---
+++ springapp/src/main/java/com/ipartek/formacion/springapp/controladores/PublicoController.java
@@ -0,0 +1,34 @@
+package com.ipartek.formacion.springapp.controladores;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ipartek.formacion.springapp.servicios.AnonimoService;
+
+@Controller
+@RequestMapping("/")
+public class PublicoController {
+	@Autowired
+	private AnonimoService servicio;
+	
+	@GetMapping
+	public String inicio(Model modelo) {
+		var productos = servicio.listarProductos();
+		
+		modelo.addAttribute("productos", productos);
+		
+		return "index";
+	}
+	
+	@GetMapping("/detalle")
+	public String detalle(Long id, Model modelo) {
+		var producto = servicio.buscarProductoPorId(id);
+		
+		modelo.addAttribute("producto", producto);
+		
+		return "detalle";
+	}
+}
