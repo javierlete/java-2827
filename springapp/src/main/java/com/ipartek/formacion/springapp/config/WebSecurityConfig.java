6188d2d796eef571fdee880d1d14880dbd7a14c4
Javier Lete <javier@eskura21.com>
2025-05-12 11:21:08 +0200

    Autenticación por medio de JDBC (MySQL)

--- springapp/src/main/java/com/ipartek/formacion/springapp/config/WebSecurityConfig.java
+++ springapp/src/main/java/com/ipartek/formacion/springapp/config/WebSecurityConfig.java
@@ -1,38 +1,34 @@
 package com.ipartek.formacion.springapp.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
 	// AUTENTICACIÓN
+	@Autowired
+	private DataSource dataSource;
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth)
+	  throws Exception {
+	    auth.jdbcAuthentication().dataSource(dataSource);
+	}
+	
 	@Bean
-	UserDetailsService userDetailsService() {
-		UserDetails admin =
-			 User.withDefaultPasswordEncoder()
-				.username("javier")
-				.password("lete")
-				.roles("ADMINISTRADOR", "USUARIO")
-				.build();
-		
-		UserDetails pepe =
-			 User.withDefaultPasswordEncoder()
-				.username("pepe")
-				.password("perez")
-				.roles("USUARIO")
-				.build();
-			
-		return new InMemoryUserDetailsManager(admin, pepe);
+	PasswordEncoder passwordEncoder() {
+	    return NoOpPasswordEncoder.getInstance(); //new BCryptPasswordEncoder();
 	}
 	
 	// AUTORIZACIÓN
