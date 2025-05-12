4c8b5f093f2a76c5b5bf408f60e5fbc4c76ecf9c
Javier Lete <javier@eskura21.com>
2025-05-12 10:14:08 +0200

    Autenticación hardcodeada y autorización

---
+++ springapp/src/main/java/com/ipartek/formacion/springapp/config/WebSecurityConfig.java
@@ -0,0 +1,53 @@
+package com.ipartek.formacion.springapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+	// AUTENTICACIÓN
+	@Bean
+	UserDetailsService userDetailsService() {
+		UserDetails admin =
+			 User.withDefaultPasswordEncoder()
+				.username("javier")
+				.password("lete")
+				.roles("ADMINISTRADOR", "USUARIO")
+				.build();
+		
+		UserDetails pepe =
+				 User.withDefaultPasswordEncoder()
+					.username("pepe")
+					.password("perez")
+					.roles("USUARIO")
+					.build();
+			
+		return new InMemoryUserDetailsManager(admin, pepe);
+	}
+	
+	// AUTORIZACIÓN
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+			.authorizeHttpRequests(requests -> requests
+				.requestMatchers("/admin/**").hasRole("ADMINISTRADOR")
+				.requestMatchers("/detalle").authenticated() //hasAnyRole("USUARIO", "ADMINISTRADOR")
+				.anyRequest().permitAll()
+			)
+			.formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
+			.logout(LogoutConfigurer::permitAll);
+
+		return http.build();
+	}
+
+}
