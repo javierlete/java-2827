26a81b3c4efa48cc74d4fb3dd83c80d61aab2e9b
Desarrollo <Desarrollo@192.168.0.105>
2025-05-15 10:04:43 +0200

    Creación de modelo de carrito

---
+++ springapp/src/main/java/com/ipartek/formacion/springapp/modelos/Carrito.java
@@ -0,0 +1,50 @@
+package com.ipartek.formacion.springapp.modelos;
+
+import java.math.BigDecimal;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import com.ipartek.formacion.springapp.entidades.Producto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+public class Carrito {
+	private Map<Long, LineaCarrito> lineas = new LinkedHashMap<>();
+
+	public Iterable<LineaCarrito> getLineas() {
+		return Collections.unmodifiableCollection(lineas.values());
+	}
+
+	public LineaCarrito getLineaPorId(Long id) {
+		return lineas.get(id);
+	}
+
+	public void ponerLinea(LineaCarrito linea) {
+		lineas.put(linea.getProducto().getId(), linea);
+	}
+
+	public void quitarLinea(Long id) {
+		lineas.remove(id);
+	}
+
+	public BigDecimal getTotal() {
+		return lineas.values().stream().map(l -> l.getTotal()).reduce(BigDecimal.ZERO,
+				(totalParcial, acumulado) -> acumulado.add(totalParcial));
+	}
+
+	@Builder
+	@NoArgsConstructor
+	@AllArgsConstructor
+	@Data
+	public static class LineaCarrito {
+		private Producto producto;
+		private Integer cantidad;
+
+		public BigDecimal getTotal() {
+			return producto.getPrecio().multiply(new BigDecimal(cantidad));
+		}
+	}
+}
