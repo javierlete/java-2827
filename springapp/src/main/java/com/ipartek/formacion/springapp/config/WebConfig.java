12348ed65f936f278d52a1a6776184f93382d09f
Desarrollo <Desarrollo@192.168.0.105>
2025-05-15 08:57:05 +0200

    Eliminación de warnings del log de Spring

--- springapp/src/main/java/com/ipartek/formacion/springapp/config/WebConfig.java
+++ springapp/src/main/java/com/ipartek/formacion/springapp/config/WebConfig.java
@@ -16,7 +16,7 @@ public class WebConfig implements WebMvcConfigurer {
 	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler(urlImagenes + "/**")
+		registry.addResourceHandler(urlImagenes + "**")
 			.addResourceLocations("file:" + rutaImagenes);
 	}
 	
