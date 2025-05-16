bcc35bf63409bb817d205cf8152c1d88ca7cd4f8
Desarrollo <Desarrollo@192.168.0.105>
2025-05-16 12:17:55 +0200

    Receptor de cambios de idioma por cookie

--- springapp/src/main/java/com/ipartek/formacion/springapp/config/WebConfig.java
+++ springapp/src/main/java/com/ipartek/formacion/springapp/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.ipartek.formacion.springapp.config;
 import java.util.logging.Level;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
@@ -10,8 +11,10 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 
 import com.ipartek.formacion.springapp.modelos.Carrito;
 
@@ -21,6 +24,7 @@ import lombok.extern.java.Log;
 @Configuration
 @ControllerAdvice
 public class WebConfig implements WebMvcConfigurer {
+
 	@Value("${rutas.imagenes.url}")
 	private String urlImagenes;
 	
@@ -52,4 +56,9 @@ public class WebConfig implements WebMvcConfigurer {
 		
         return "errores/generico";
     }
+	
+	@Bean
+	LocaleResolver localeResolver() {
+		return new CookieLocaleResolver("idioma");
+	}
 }
