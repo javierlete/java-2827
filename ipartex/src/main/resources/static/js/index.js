3c14495abba711d593bc4edbd560d6c7c305eb63
Desarrollo <Desarrollo@192.168.0.105>
2025-05-22 10:01:33 +0200

    Enviamos sólo las peticiones necesarias

--- ipartex/src/main/resources/static/js/index.js
+++ ipartex/src/main/resources/static/js/index.js
@@ -5,6 +5,8 @@ const URL_MENSAJES = URL + '/mensajes/'
 const TAMANO_PAGINA = 20;
 
 let numero_pagina = 0;
+let ultima = false;
+
 let ul;
 
 window.addEventListener('DOMContentLoaded', async () => {
@@ -12,29 +14,35 @@ window.addEventListener('DOMContentLoaded', async () => {
 	ul.innerHTML = '';
 
 	await cargarMensajes();
-	
+
 	const masMensajes = document.querySelector('#mas-mensajes');
-	
+
 	masMensajes.addEventListener('click', evento => {
 		evento.preventDefault();
-		
+
 		numero_pagina++;
-		
+
 		cargarMensajes();
 	});
 });
 
 async function cargarMensajes() {
-    const respuesta = await fetch(`${URL_MENSAJES}?page=${numero_pagina}&size=${TAMANO_PAGINA}`);
-    const resultado = await respuesta.json();
+	if (ultima) {
+		return;
+	}
 
-    resultado.content.forEach(mensaje => {
-        const li = document.createElement('li');
+	const respuesta = await fetch(`${URL_MENSAJES}?page=${numero_pagina}&size=${TAMANO_PAGINA}`);
+	const resultado = await respuesta.json();
 
-        li.innerHTML = `
+	ultima = resultado.last;
+
+	resultado.content.forEach(mensaje => {
+		const li = document.createElement('li');
+
+		li.innerHTML = `
 			${mensaje.usuarioNombre}, ${mensaje.fecha}, ${mensaje.texto}
 		`;
 
-        ul.appendChild(li);
-    });
+		ul.appendChild(li);
+	});
 }
