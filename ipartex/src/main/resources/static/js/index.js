ac640474d21db72b48e847b668a308577f017436
Desarrollo <Desarrollo@192.168.0.105>
2025-05-22 09:57:18 +0200

    Más mensajes funcional básico

--- ipartex/src/main/resources/static/js/index.js
+++ ipartex/src/main/resources/static/js/index.js
@@ -2,22 +2,39 @@
 
 const URL = '/api/v2'
 const URL_MENSAJES = URL + '/mensajes/'
+const TAMANO_PAGINA = 20;
+
+let numero_pagina = 0;
+let ul;
 
 window.addEventListener('DOMContentLoaded', async () => {
-	const respuesta = await fetch(URL_MENSAJES);
-	const resultado = await respuesta.json();
-	
-	const ul = document.querySelector('#mensajes');
-	
+	ul = document.querySelector('#mensajes');
 	ul.innerHTML = '';
+
+	await cargarMensajes();
 	
-	resultado.content.forEach(mensaje => {
-		const li = document.createElement('li');
+	const masMensajes = document.querySelector('#mas-mensajes');
+	
+	masMensajes.addEventListener('click', evento => {
+		evento.preventDefault();
 		
-		li.innerHTML = `
-			${mensaje.usuarioNombre}, ${mensaje.fecha}, ${mensaje.texto}
-		`;
+		numero_pagina++;
 		
-		ul.appendChild(li);
+		cargarMensajes();
 	});
 });
+
+async function cargarMensajes() {
+    const respuesta = await fetch(`${URL_MENSAJES}?page=${numero_pagina}&size=${TAMANO_PAGINA}`);
+    const resultado = await respuesta.json();
+
+    resultado.content.forEach(mensaje => {
+        const li = document.createElement('li');
+
+        li.innerHTML = `
+			${mensaje.usuarioNombre}, ${mensaje.fecha}, ${mensaje.texto}
+		`;
+
+        ul.appendChild(li);
+    });
+}
