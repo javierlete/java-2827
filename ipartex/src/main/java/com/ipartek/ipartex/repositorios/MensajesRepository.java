b2f9ba0b2434430bfe9270ae1645dfaa218e3556
Desarrollo <Desarrollo@192.168.0.105>
2025-05-22 08:37:11 +0200

    Controladores REST automáticos de Spring Data REST

--- ipartex/src/main/java/com/ipartek/ipartex/repositorios/MensajesRepository.java
+++ ipartex/src/main/java/com/ipartek/ipartex/repositorios/MensajesRepository.java
@@ -5,10 +5,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.ipartek.ipartex.entidades.Mensaje;
 
+@RepositoryRestResource(collectionResourceRel = "mensajes", path="mensajes")
 public interface MensajesRepository extends CrudRepository<Mensaje, Long>, PagingAndSortingRepository<Mensaje, Long> {
+	@RestResource(exported = false)
 	@Query("from Mensaje m join fetch m.autor order by m.fecha desc")
 	Iterable<Mensaje> listadoMensajes();
 	
--- ipartex/src/main/java/com/ipartek/ipartex/repositorios/UsuariosRepository.java
+++ ipartex/src/main/java/com/ipartek/ipartex/repositorios/UsuariosRepository.java
@@ -1,9 +1,11 @@
 package com.ipartek.ipartex.repositorios;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.ipartek.ipartex.entidades.Usuario;
 
+@RepositoryRestResource(collectionResourceRel = "usuarios", path = "usuarios")
 public interface UsuariosRepository extends CrudRepository<Usuario, Long> {
 	Usuario findByEmail(String email);
 }
