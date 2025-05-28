110fee86f3ac09e7473670541cf13838d3d80c7a
Javier <javier@eskura21.com>
2025-05-28 10:33:07 +0200

    Colocamos rutas y visualizamos listado en app

--- springapp-angular/src/app/app.routes.ts
+++ springapp-angular/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ListadoComponent } from './componentes/listado/listado.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'listado', pathMatch: 'full' },
+    { path: 'listado', component: ListadoComponent }
+];
