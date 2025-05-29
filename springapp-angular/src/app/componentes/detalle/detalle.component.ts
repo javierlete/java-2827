fe1a631a0efb49809d2bf8878e138a8a591bd408
Javier <javier@eskura21.com>
2025-05-29 09:54:51 +0200

    Detalle utilizando ficha en horizontal

--- springapp-angular/src/app/componentes/detalle/detalle.component.ts
+++ springapp-angular/src/app/componentes/detalle/detalle.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { FichaComponent } from "../ficha/ficha.component";
 
 @Component({
   selector: 'app-detalle',
-  imports: [],
+  imports: [FichaComponent],
   templateUrl: './detalle.component.html',
   styleUrl: './detalle.component.css'
 })
 export class DetalleComponent {
-
+  
 }
--- springapp-angular/src/app/componentes/ficha/ficha.component.ts
+++ springapp-angular/src/app/componentes/ficha/ficha.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -8,5 +8,5 @@ import { RouterLink } from '@angular/router';
   styleUrl: './ficha.component.css'
 })
 export class FichaComponent {
-
+  @Input() horizontal = false;
 }
