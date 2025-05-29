8602e1f9bf031b69a501b1ee1068e5adaf835b03
Javier <javier@eskura21.com>
2025-05-29 10:23:29 +0200

    Acoplamos el admin-listado a la aplicación

--- springapp-angular/src/app/app.routes.ts
+++ springapp-angular/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { ListadoComponent } from './componentes/listado/listado.component';
 import { DetalleComponent } from './componentes/detalle/detalle.component';
+import { AdminListadoComponent } from './componentes/admin-listado/admin-listado.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'listado', pathMatch: 'full' },
     { path: 'listado', component: ListadoComponent },
     { path: 'detalle', component: DetalleComponent },
+    { path: 'admin/listado', component: AdminListadoComponent },
 ];
