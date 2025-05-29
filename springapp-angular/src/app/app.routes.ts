5a744b7a0e8840b7a9cfbf3cccf38304724e5f34
Javier <javier@eskura21.com>
2025-05-29 11:48:17 +0200

    Fin de simulacro superficial

--- springapp-angular/src/app/app.routes.ts
+++ springapp-angular/src/app/app.routes.ts
@@ -4,12 +4,21 @@ import { DetalleComponent } from './componentes/publica/detalle/detalle.componen
 import { AdminListadoComponent } from './componentes/admin/admin-listado/admin-listado.component';
 import { AdminDetalleComponent } from './componentes/admin/admin-detalle/admin-detalle.component';
 import { LoginComponent } from './componentes/publica/login/login.component';
+import { CarritoComponent } from './componentes/publica/carrito/carrito.component';
 
 export const routes: Routes = [
+    // Entrada inicial
     { path: '', redirectTo: 'listado', pathMatch: 'full' },
+    
+    // Login
     { path: 'login', component: LoginComponent },
+    
+    // Público
     { path: 'listado', component: ListadoComponent },
     { path: 'detalle', component: DetalleComponent },
+    { path: 'carrito', component: CarritoComponent },
+
+    // Administración
     { path: 'admin/listado', component: AdminListadoComponent },
     { path: 'admin/detalle', component: AdminDetalleComponent },
 ];
