fce01643f2a109a57a2b63e3ecd5774027b50ba3
Javier <javier@eskura21.com>
2025-05-29 11:33:47 +0200

    Trasladamos a carpetas public y admin

--- springapp-angular/src/app/app.routes.ts
+++ springapp-angular/src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
-import { ListadoComponent } from './componentes/listado/listado.component';
-import { DetalleComponent } from './componentes/detalle/detalle.component';
-import { AdminListadoComponent } from './componentes/admin-listado/admin-listado.component';
-import { AdminDetalleComponent } from './componentes/admin-detalle/admin-detalle.component';
-import { LoginComponent } from './componentes/login/login.component';
+import { ListadoComponent } from './componentes/publica/listado/listado.component';
+import { DetalleComponent } from './componentes/publica/detalle/detalle.component';
+import { AdminListadoComponent } from './componentes/admin/admin-listado/admin-listado.component';
+import { AdminDetalleComponent } from './componentes/admin/admin-detalle/admin-detalle.component';
+import { LoginComponent } from './componentes/publica/login/login.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'listado', pathMatch: 'full' },
