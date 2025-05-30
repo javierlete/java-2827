74916de890ce64109a7c7d05b418dde0445e29c8
Javier <javier@eskura21.com>
2025-05-30 11:35:03 +0200

    jl-menu

--- springapp-angular/src/app/app.component.ts
+++ springapp-angular/src/app/app.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
+import { Enlace, MenuComponent } from "./componentes/biblioteca/menu/menu.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'springapp-angular';
+
+  enlaces: Enlace[] = [
+    { texto: 'Principal', link: '/listado' },
+    { texto: 'Admin Productos', link: '/admin/listado', posicion: 'derecha' },
+    { texto: 'No estás autenticado', posicion: 'derecha' },
+    { texto: 'Usuario', posicion: 'derecha' },
+    { texto: 'Roles', posicion: 'derecha' },
+    { texto: 'Login', link: '/login', posicion: 'derecha' },
+    { texto: 'Cierre de sesión', link: '/logout', posicion: 'derecha' },
+    { texto: 'es', link: '/es', posicion: 'derecha' },
+    { texto: 'en', link: '/en', posicion: 'derecha' },
+  ]
 }
--- springapp-angular/src/app/componentes/biblioteca/menu/menu.component.ts
+++ springapp-angular/src/app/componentes/biblioteca/menu/menu.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'jl-menu',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
+  @Input() titulo: string = 'TITULO';
+  @Input() expansion: 'sm'|'md'|'lg'|'xl'|'xxl' = 'lg';
+  @Input() enlaces: Enlace[] = [];
 
+  enlacesPosicion(posicion?: 'derecha') {
+    if(!posicion) {
+      return this.enlaces.filter(e => !e.posicion);
+    } else {
+      return this.enlaces.filter(e => e.posicion === posicion);
+    }
+  }
+}
+
+export interface Enlace {
+  texto: string;
+  link?: string;
+  posicion?: 'derecha';
+
+  subenlaces?: Enlace[];
 }
