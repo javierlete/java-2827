22fec74070cfa58fe35f3220e74d964fdfb4eb9b
Javier <javier@eskura21.com>
2025-05-30 09:47:29 +0200

    ng g c biblioteca/boton

---
+++ springapp-angular/src/app/componentes/biblioteca/boton/boton.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-boton',
+  imports: [],
+  templateUrl: './boton.component.html',
+  styleUrl: './boton.component.css'
+})
+export class BotonComponent {
+
+}
