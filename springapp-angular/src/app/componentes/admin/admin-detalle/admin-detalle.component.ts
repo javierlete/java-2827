e7663fbcb645d67d1341001ca82bce0f8d3aba96
Javier <javier@eskura21.com>
2025-05-30 08:39:39 +0200

    Vuelta del formulario en el simulacro

--- springapp-angular/src/app/componentes/admin/admin-detalle/admin-detalle.component.ts
+++ springapp-angular/src/app/componentes/admin/admin-detalle/admin-detalle.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-admin-detalle',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './admin-detalle.component.html',
   styleUrl: './admin-detalle.component.css'
 })
