e2ebb60f674eb79f7b995cecca19960c0f2ea04f
Javier <javier@eskura21.com>
2025-05-30 10:14:56 +0200

    ng g c biblioteca/alerta

---
+++ springapp-angular/src/app/componentes/biblioteca/alerta/alerta.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alerta',
+  imports: [],
+  templateUrl: './alerta.component.html',
+  styleUrl: './alerta.component.css'
+})
+export class AlertaComponent {
+
+}
