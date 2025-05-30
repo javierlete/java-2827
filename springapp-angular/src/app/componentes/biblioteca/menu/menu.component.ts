0fcf50a136b6687e85ff7726a5490f3abdfeead9
Javier <javier@eskura21.com>
2025-05-30 10:49:19 +0200

    ng g c biblioteca/menu --selector jl-menu

---
+++ springapp-angular/src/app/componentes/biblioteca/menu/menu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jl-menu',
+  imports: [],
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.css'
+})
+export class MenuComponent {
+
+}
