734d5dc097c8e7d27ea3346af30bebf77ad2c686
Javier <javier@eskura21.com>
2025-06-12 12:16:46 +0200

    Script para hacer CI/CD

---
+++ springapp-deploy.bat
@@ -0,0 +1,7 @@
+git push && ^
+cd springapp && ^
+mvnw package -Dmaven.test.skip=true && ^
+docker build -t javierlete/springapp . && ^
+docker push javierlete/springapp && ^
+curl https://api.render.com/deploy/srv-d14id663jp1c73betsfg?key=RwTQNFbAw80 && ^
+cd .. 
