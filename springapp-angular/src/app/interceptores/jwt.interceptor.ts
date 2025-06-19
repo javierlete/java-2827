9c4ed875b4f0d0aedc543f673014b59702f8cec5
Javier <javier@eskura21.com>
2025-06-19 12:14:00 +0200

    Autenticación de cliente Angular

--- springapp-angular/src/app/interceptores/jwt.interceptor.ts
+++ springapp-angular/src/app/interceptores/jwt.interceptor.ts
@@ -1,5 +1,15 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
+  const token = localStorage.getItem('token');
+  
+  if (!token) {
+    return next(req);
+  }
+
+  const conToken = req.clone({
+      headers: req.headers.set('Authorization', 'Bearer ' + localStorage.getItem('token'))
+  });
+  
+  return next(conToken);
 };
--- springapp-angular/src/app/servicios/autenticacion.service.ts
+++ springapp-angular/src/app/servicios/autenticacion.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
+import { Login } from '../modelos/login';
+import { HttpClient } from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AutenticacionService {
 
-  constructor() { }
+  constructor(private readonly http: HttpClient) { }
+
+  // Autentica al usuario y devuelve un token JWT
+  autenticar(login: Login): Observable<string> {
+    return this.http.post<string>('http://localhost:8080/api/v2/auth/login', login).pipe(
+      tap(token => localStorage.setItem('token', token))
+    );
+  }
+
+  logout(): void {
+    localStorage.removeItem('token');
+  }
+
+  usuarioAutenticado(): string | null {
+    const token = localStorage.getItem('token');
+    
+    if (!token) {
+      return null; // Si no hay token, no hay usuario autenticado
+    }
+    
+    return JSON.parse(atob(token.split('.')[1])).sub;
+  }
+
+  autenticado(): boolean {
+    const token = localStorage.getItem('token');
+    return !!token; // Devuelve true si hay un token, false si no
+  }
+
+  rol(): string | null {
+    return 'ADMINISTRADOR';
+  }
 }
