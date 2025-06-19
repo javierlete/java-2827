4f8d02770dc81bb8c0e28c4380386ffaa5e87b69
Javier <javier@eskura21.com>
2025-06-19 12:16:07 +0200

    Login funcional desde componente

--- springapp-angular/src/app/componentes/publica/login/login.component.ts
+++ springapp-angular/src/app/componentes/publica/login/login.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { LabelInputComponent } from "../../biblioteca/label-input/label-input.component";
 import { AlertaComponent } from "../../biblioteca/alerta/alerta.component";
+import { Login } from '../../../modelos/login';
+import { AutenticacionService } from '../../../servicios/autenticacion.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,5 +12,17 @@ import { AlertaComponent } from "../../biblioteca/alerta/alerta.component";
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  login: Login = {
+    email: 'javier@email.net',
+    password: 'javier'
+  };
+  
+  constructor(private readonly autenticacionService: AutenticacionService, private readonly router: Router) { }
+
+  autenticar() {
+    console.log('Intentando autenticar con:', this.login);
+
+    this.autenticacionService.autenticar(this.login).subscribe(() => this.router.navigate(['/']));
+  }
 
 }
