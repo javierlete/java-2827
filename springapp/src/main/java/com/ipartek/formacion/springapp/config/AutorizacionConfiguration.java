fe853e42609b6859c4b40f11919a3686125ecbc9
Javier <javier@eskura21.com>
2025-06-19 12:10:27 +0200

    Activamos seguridad de métodos y permitimos peticiones desde cualquier servidor (CORS) y a cualquier ruta

--- springapp/src/main/java/com/ipartek/formacion/springapp/config/AutorizacionConfiguration.java
+++ springapp/src/main/java/com/ipartek/formacion/springapp/config/AutorizacionConfiguration.java
@@ -1,14 +1,21 @@
 package com.ipartek.formacion.springapp.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.ipartek.formacion.springapp.filtros.JwtAuthenticationFilter;
 
@@ -16,6 +23,7 @@ import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 @RequiredArgsConstructor
 public class AutorizacionConfiguration {
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
@@ -25,14 +33,26 @@ public class AutorizacionConfiguration {
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(authRequest -> authRequest
-                        .requestMatchers("/api/v2/auth/**").permitAll()
-                        .requestMatchers("/api/**").authenticated()
                         .anyRequest().permitAll())
                 .sessionManagement(sessionManager -> sessionManager
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authProvider)
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOriginPatterns(List.of("*")); // Permite cualquier origen
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true); // Solo si necesitas enviar cookies
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 }
--- springapp/src/main/java/com/ipartek/formacion/springapp/servicios/AdminService.java
+++ springapp/src/main/java/com/ipartek/formacion/springapp/servicios/AdminService.java
@@ -1,5 +1,6 @@
 package com.ipartek.formacion.springapp.servicios;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 
 import com.ipartek.formacion.springapp.entidades.Producto;
@@ -8,11 +9,14 @@ import jakarta.validation.Valid;
 
 @Validated
 public interface AdminService extends AnonimoService {
-
+	
+	@PreAuthorize("hasRole('ADMINISTRADOR')")
 	void anyadirProducto(@Valid Producto producto);
-
+	
+	@PreAuthorize("hasRole('ADMINISTRADOR')")
 	void modificarProducto(@Valid Producto producto);
-
+	
+	@PreAuthorize("hasRole('ADMINISTRADOR')")
 	void borrarProducto(Long id);
 
 }
