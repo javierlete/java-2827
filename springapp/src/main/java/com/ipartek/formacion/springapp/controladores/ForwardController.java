0a6e9e74e57c930c7078842f291bc13821de0b96
Javier <javier@eskura21.com>
2025-06-23 13:07:30 +0200

    Pasamos a GetMapping el ForwardController

--- springapp/src/main/java/com/ipartek/formacion/springapp/controladores/ForwardController.java
+++ springapp/src/main/java/com/ipartek/formacion/springapp/controladores/ForwardController.java
@@ -1,15 +1,15 @@
 package com.ipartek.formacion.springapp.controladores;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.java.Log;
 
 @Log
 @Controller
 public class ForwardController {
-    @RequestMapping(value = "/{path:[^.]*}/**")
+    @GetMapping(value = "/{path:[^.\\-]*}/**")
     public String forward(@PathVariable String path) {
         log.info("Forwarding request for path: " + path);
         return "forward:/index.html";
